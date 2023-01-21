de9b26df6bbb1bf59ffbab81f5d82584a2c2560f
HalkoZ <111779806+HalkoZ@users.noreply.github.com>
2023-01-21 23:33:57 +0100

    Quotes
    Quotes

---
+++ Quotes websites/script.js
@@ -0,0 +1,39 @@
+var quotes = [];
+var allQuotes = [];
+
+function renderQuotes() {
+  var parentEl = document.getElementById("list");
+  parentEl.innerHTML = "";
+  quotes.forEach(function (item, index) {
+    var childEl = document.createElement("li");
+    childEl.textContent =
+      item.quoteText + " - " + item.quoteAuthor + " - " + item.quoteSource;
+    parentEl.appendChild(childEl);
+  });
+}
+
+fetch("https://js-course-server.onrender.com/quotes/get-all-quotes")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    quotes = data;
+    allQuotes = data;
+    renderQuotes();
+    console.log("data", data);
+  })
+  .catch(function (err) {
+    console.log("err", err);
+  });
+
+document.getElementById("search").addEventListener("keyup", function () {
+  var searchValue = document.getElementById("search").value;
+  quotes = allQuotes.filter(function (item, index) {
+    if (item.quoteText) {
+      return item.quoteText.toLowerCase().includes(searchValue.toLowerCase());
+    } else {
+      return false;
+    }
+  });
+  renderQuotes();
+});
